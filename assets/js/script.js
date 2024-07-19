2b958c47e9bf6e09a772f98a4e9e81bcf5e7613a
Luis Angel <angeltalamantelara@gmail.com>
2024-07-19 12:09:52 -0700

    PRIMER

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 
-
 /**
  * PRELOAD
  * 
@@ -168,3 +167,44 @@ window.addEventListener("mousemove", function (event) {
   }
 
 });
+
+function initMap() {
+  const myLatlng = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatlng,
+  });
+  const marker = new google.maps.Marker({
+    position: myLatlng,
+    map,
+    title: "Click to zoom",
+  });
+
+  map.addListener("center_changed", () => {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(() => {
+      map.panTo(marker.getPosition());
+    }, 3000);
+  });
+  marker.addListener("click", () => {
+    map.setZoom(8);
+    map.setCenter(marker.getPosition());
+  });
+}
+
+window.initMap = initMap;
+
+/*
+function iniciarMap(){
+  var coord = {lat:-110.9990309 ,lng: -110.9964506};
+  var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 10,
+    center: coord
+  });
+  var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
+}
+  */
