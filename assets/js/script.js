13977798e8e82c967c3c73471d578f16f3f9da88
Luis Angel <angeltalamantelara@gmail.com>
2024-08-04 16:50:30 -0700

    asd

--- assets/js/script.js
+++ assets/js/script.js
@@ -480,17 +480,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-/*
-function iniciarMap(){
-  var coord = {lat:-110.9990309 ,lng: -110.9964506};
-  var map = new google.maps.Map(document.getElementById('map'),{
-    zoom: 10,
-    center: coord
-  });
-  var marker = new google.maps.Marker({
-    position: coord,
-    map: map
-  });
-}
-  */
- 
